dc26fa95d98954bd7091eaa7d62c38b7e46dc066
Blazze23 <l.azmar23@gmail.com>
2022-01-13 01:59:03 +0100

    Fixed navigation bug when clicking on logo to display active class on home

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use-strict";
 
+const nav = document.querySelector(".nav");
 const navMenu = document.getElementById("nav-menu");
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
@@ -17,14 +18,21 @@ if (navClose) {
   });
 }
 
-navMenu.addEventListener("click", (e) => {
+nav.addEventListener("click", (e) => {
   navLinks.forEach((link) => {
     link.classList.remove("nav__link-active");
   });
+
+  //   When we click on links
   if (e.target.classList.contains("nav__link")) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView();
     e.target.classList.add("nav__link-active");
     navMenu.classList.remove("show__nav-menu");
+  }
+
+  //   When we click on logo
+  if (e.target.classList.contains("logo__img")) {
+    navLinks[0].classList.add("nav__link-active");
   }
 });
